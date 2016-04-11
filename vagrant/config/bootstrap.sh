587a7cd9d3051c7efd666f9ae7f64a68ded12474
Rodrigo Lins <rodrigolinsr@gmail.com>
2016-04-11 10:04:38 -0300

    Fixing Vagrant configuration

--- vagrant/config/bootstrap.sh
+++ vagrant/config/bootstrap.sh
@@ -34,6 +34,7 @@ rm /etc/nginx/sites-available/default
 cp /code/simple-blog/vagrant/config/default /etc/nginx/sites-available/default
 cp /code/simple-blog/vagrant/config/simple-blog /etc/nginx/sites-available/simple-blog
 ln -s /etc/nginx/sites-available/simple-blog /etc/nginx/sites-enabled/simple-blog
+cp /code/simple-blog/vagrant/config/.env /code/simple-blog/.env
 
 sed -i 's/user = www-data/user = vagrant/g' /etc/php/7.0/fpm/pool.d/www.conf
 sed -i 's/owner = www-data/owner = vagrant/g' /etc/php/7.0/fpm/pool.d/www.conf
@@ -42,13 +43,18 @@ sed -i 's/user www-data/user vagrant/g' /etc/nginx/nginx.conf
 
 /etc/init.d/nginx restart
 
+# Install MongoDB
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
+apt-get install -y mongodb-org
 
+# Install PHP extension for MongoDB
 apt-get install -y php-pear libcurl4-openssl-dev pkg-config
 pecl install mongodb
 echo "extension=mongodb.so" >> /etc/php/7.0/fpm/php.ini
+echo "extension=mongodb.so" >> /etc/php/7.0/cli/php.ini
+sudo service php7.0-fpm restart
 
 # Composer
 curl -sS https://getcomposer.org/installer | php
@@ -57,10 +63,13 @@ mv composer.phar /usr/local/bin/composer
 chmod -R 775 /code/simple-blog/storage
 chmod -R 775 /code/simple-blog/bootstrap/cache
 
+# Initialize project
 cd /code/simple-blog
 su vagrant -c "composer install"
 
 sudo service php7.0-fpm restart
 
-# su vagrant -c "php artisan migrate"
-# su vagrant -c "php artisan db:seed"
+mongo simpleBlog --eval "db.createUser({user: 'simpleBlogAdmin', pwd: 'p4ssw0rd', roles: [{role: 'userAdmin', db: 'simpleBlog'}]})"
+
+su vagrant -c "php artisan migrate"
+su vagrant -c "php artisan db:seed"
