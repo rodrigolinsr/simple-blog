afcf6b71c5635b3ebcfc45689b6f9a0c0feab5c5
Rodrigo Lins <rodrigolinsr@gmail.com>
2016-04-11 13:45:07 -0300

    Put dynamic content on index

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,6 +6,7 @@ use App\Http\Requests;
 use Illuminate\Http\Request;
 
 use App\MyLib\BlogSettings;
+use App\Models\{Post, Category, Tag};
 
 class IndexController extends Controller
 {
@@ -13,7 +14,20 @@ class IndexController extends Controller
     {
       $blogSettingsIni = BlogSettings::getSettings();
 
+      /*
+       * TODO:
+       * 1) Filter by non-draft
+       * 2) Order by updated_at
+       * 3) Rank tags
+       */
+      $posts = Post::all();
+      $categories = Category::all();
+      $tags = Tag::all();
+
       return view('index')->with('welcomeTitle', $blogSettingsIni['welcome_title'])
-                          ->with('welcomeMessage', $blogSettingsIni['welcome_message']);
+                          ->with('welcomeMessage', $blogSettingsIni['welcome_message'])
+                          ->with('posts', $posts)
+                          ->with('categories', $categories)
+                          ->with('tags', $tags);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,43 +12,69 @@
 @endif
 <div class="row">
   <div class="col-md-9 posts-column">
-    <article id="post-19" class="post type-post status-publish format-standard">
-      <header class="entry-header">
-        <h2 class="entry-title"><a href="#" title="My first post">My first post</a></h2>
-        <div class="entry-meta">
-          <span class="posted-on">Published on
-            <span class="entry-date text-danger">April 10, 2016</span>
-          </span>
-          <span class="by-line"> by
-            <span class="author"><a href="mailto:rodrigolinsr@gmail.com">Rodrigo Lins</a></span>
-          </span>
-          <br />
-          <span class="category-name">Category:
-            <span class="">&nbsp;<a href="#" rel="category">Uncategorized</a></span>
-          </span>
-        </div>
-      </header>
-      <div class="entry-content">
-        {!! file_get_contents('http://loripsum.net/api/10/short/headers') !!}
-      </div>
-    </article>
+    <!-- Posts -->
+    @if(!count($posts))
+      <h2>No posts found.</h2>
+      @if(!Auth::guest())
+        <h4><a href="{{ action('Admin\PostsController@create') }}">Click here</a> to write your first post!</h4>
+      @endif
+    @else
+      @foreach($posts as $post)
+        <article id="post-{{ $post->id }}">
+          <header class="entry-header">
+            <h2 class="entry-title"><a href="#" title="{{ $post->title }}">{{ $post->title }}</a></h2>
+            <div class="entry-meta">
+              <span class="posted-on">Published on
+                <span class="entry-date text-danger">{{ date('F d, Y', strtotime($post->updated_at)) }}</span>
+              </span>
+              <span class="by-line"> by
+                <span class="author"><a href="#">{{ $post->author->name }}</a></span>
+              </span>
+              <br />
+              <span class="category-name">Categories:
+                <span class="">
+                  @if(!count($post->categories))
+                  <a href="#" rel="category">Uncategorized</a>
+                  @else
+                    @foreach($post->categories as $category)
+                      <a href="#" rel="category">{{ $category->name }}</a>,
+                    @endforeach
+                  @endif
+                </span>
+              </span>
+            </div>
+          </header>
+          <div class="entry-content">
+            {!! $post->text !!}
+          </div>
+        </article>
+      @endforeach
+    @endif
   </div>
   <div class="col-md-3 sidebar-column">
+    <!-- Categories -->
     <div class="category-list">
       <h3>Categories</h3>
       <ul>
         <li>Uncategorized</li>
-        @for ($i = 1; $i <= 5; $i++)
-          <li><a href="#">Category {{ $i }}</a></li>
-        @endfor
+        @if(count($categories))
+          @foreach($categories as $category)
+            <li><a href="#">{{ $category->name }}</a></li>
+          @endforeach
+        @endif
       </ul>
     </div>
+    <!-- Tags -->
     <div class="tag-cloud">
       <h3>Tags</h3>
       <ul>
-        @for ($i = 1; $i <= 20; $i++)
-          <li><a href="#" class="tag-rank-{{ rand(1,10) }}">Tag{{ $i }}</a></li>
-        @endfor
+        @if(!count($tags))
+        <li>No tags available.</li>
+        @else
+          @foreach($tags as $tag)
+            <li><a href="#" class="tag-rank-{{ rand(1,10) }}">{{ $tag->name }}</a></li>
+          @endforeach
+        @endif
       </ul>
     </div>
   </div>
